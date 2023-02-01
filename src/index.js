b448889637cd9b12a59261ca81dcd201c5267883
Craig Ford <craig_ford@icloud.com>
2023-02-01 13:20:13 -0500

    commit tree 8a1c725a53e9c4c4a209223b01e50ad1e986be13

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 // Note: createStore and candyReducer must be exported for the tests to run
 
-export function createStore() {
+export function createStore(reducer) {
   // write your createStore code here
+  let state;
+
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  }
+
+  function getState() {
+    return state;
+  }
+
+  return {
+    dispatch,
+    getState
+  };
 }
 
 export function candyReducer(state = [], action) {
@@ -22,5 +37,36 @@ function render() {
   }
 }
 
+const store = createStore(candyReducer);
+store.dispatch({ type: "@@INIT"})
+
 // Use your createStore function and the functions provided here to create a store.
 // Once the store is created, call an initial dispatch.
+
+
+
+
+
+
+
+// export function createStore() {
+//   // write your createStore code here
+// }
+
+// export function candyReducer(state = [], action) {
+//   switch (action.type) {
+//     case "candies/add":
+//       return [...state, action.candy];
+//     default:
+//       return state;
+//   }
+// }
+
+// function render() {
+//   let container = document.getElementById("container");
+//   if (store.getState()) {
+//     container.textContent = store.getState().join(" ");
+//   } else {
+//     throw new Error("the store's state has not been defined yet");
+//   }
+// }
